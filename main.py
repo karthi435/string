65b470c38b8143346056d2048fce6ee09ae99fa9
RK435 <>
2021-06-05 18:33:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#str con
+print('hello ' + 'karthi')
+print('hello ' + str(100))
+#esc seq
+weather="\t it\'s \"kind of\" cold"
+print(weather)
+
+name = 'karthik'
+age = 32
+print(f'hi {name}. you are {age} years old!')
+print('hi {}. you are {} years old!'.format('karthik', '32'))
+print('hi {new_name}. you are {age} years old!'.format(new_name='priya', age='22'))
